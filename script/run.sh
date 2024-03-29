f4ba8432a55b067dc0b46ebd2dbd36d740342a38
ShukiSasakura <sasakura2023@s.okayama-u.ac.jp>
2024-03-29 17:19:31 +0900

    Update file path

--- script/run.sh
+++ script/run.sh
@@ -4,5 +4,5 @@ echo "table:result"
 for t in $(seq 1 30)
 do
     echo -en "\t$t\t"
-    wasmer target/wasm32-wasmer-wasi/release/sum-array-elements.wasm -- -l 1000000 -c 1000 -t $t -r 100000
+    wasmer ../target/wasm32-wasmer-wasi/release/sum-array-elements.wasm -- -l 1000000 -c 1000 -t $t -r 100000
 done
